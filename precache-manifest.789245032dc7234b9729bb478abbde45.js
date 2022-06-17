2c11a77e164f1a6a03feb29c79fa7e13f9d71257
Mitch Henderson <shiftymitch@gmail.com>
2022-06-16 19:16:18 -0600

    Updates

---
+++ precache-manifest.789245032dc7234b9729bb478abbde45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc1431728dcd96d46fdd34806e56c9d2",
+    "url": "/spookygifts/index.html"
+  },
+  {
+    "revision": "1a31975105f2d772b9d2",
+    "url": "/spookygifts/static/css/main.9865c041.chunk.css"
+  },
+  {
+    "revision": "7bbd0963268dc04fe0c8",
+    "url": "/spookygifts/static/js/2.873a87b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spookygifts/static/js/2.873a87b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a31975105f2d772b9d2",
+    "url": "/spookygifts/static/js/main.4e405d76.chunk.js"
+  },
+  {
+    "revision": "7e6512f22376abe71786",
+    "url": "/spookygifts/static/js/runtime-main.e3d0cb1b.js"
+  }
+]);
